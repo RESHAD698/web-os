988e8e731acb9c1950e338e09ad5a63fb3c94d2a
RESHAD698 <ophacker1971@gmail.com>
2026-07-21 15:03:53 +0600

    Add click event to toggle lockscreen visibility

---
+++ js/fingerprint.js
@@ -0,0 +1,4 @@
+document.getElementById("fingerprint").addEventListener("click", () => {
+  document.getElementById("lockscreen").style.display = "none";
+  document.getElementById("os-shell").style.display = "block";
+});
